bdd2f264b211b56ddc6a50ded7626aab095e22a2
gabriel <cicero.pereira88@aluno.ce.gov.br>
2024-03-14 14:37:47 -0300

    k

---
+++ g.dart
@@ -0,0 +1,7 @@
+void main() {
+  //1-desenvolva um algoritimo que efetue a soma de todos os numeros impares que são multiplos de tres e que se
+//encontram no conjunto dos numeros de 1 a 100.
+void main() {
+  
+}
+}
